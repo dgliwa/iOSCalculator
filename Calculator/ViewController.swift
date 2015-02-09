bbdeac1de26beb3b2f27a6f6b00d0cbea14303cf
derekgilwa <dgliwa@thoughtworks.com>
2015-02-08 19:25:08 -0600

    Init

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -10,16 +10,64 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    @IBOutlet weak var display: UILabel!
+    
+    var userIsInMiddleOfTyping: Bool = false
+    var operandStack = Array<Double>()
+    var displayValue: Double {
+        get{
+          return NSNumberFormatter().numberFromString(display.text!)!.doubleValue
+        }
+        set{
+            display.text! = "\(newValue)"
+            userIsInMiddleOfTyping = false
+        }
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func operate(sender: UIButton) {
+        let operation = sender.currentTitle!
+        println(operandStack)
+        if(userIsInMiddleOfTyping){
+            enter()
+        }
+        switch operation {
+        case "×": performOperation {$0 * $1}
+        case "÷": performOperation {$1 / $0}
+        case "+": performOperation {$0 + $1}
+        case "−": performOperation {$1 - $0}
+        case "√": performOperation { sqrt($0) }
+            default: break
+        }
     }
-
+    
+    
+    func performOperation(operation: (Double, Double) -> Double){
+        if(operandStack.count >= 2){
+            displayValue = operation(operandStack.removeLast(),operandStack.removeLast())
+            enter()
+        }
+    }
+    
+    func performOperation(operation: (Double) -> Double){
+        if(operandStack.count >= 1){
+            displayValue = operation(operandStack.removeLast())
+            enter()
+        }
+    }
+    
+    @IBAction func appendDigit(sender: UIButton) {
+        let digit = sender.currentTitle!
+        if(userIsInMiddleOfTyping){
+            display.text = display.text! + digit
+        } else {
+            display.text = digit
+            userIsInMiddleOfTyping = true
+        }
+    }
+    @IBAction func enter() {
+        operandStack.append(displayValue)
+        userIsInMiddleOfTyping = false
+    }
 
 }
 
